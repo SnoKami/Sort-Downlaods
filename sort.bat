76e92d9a00702aa3348e4f9a168128acc6118283
aga <SnoKami@users.noreply.github.com>
2022-04-18 21:01:22 -0400

    im gay

---
+++ sort.bat
@@ -0,0 +1,104 @@
+title "Sort Downloads"
+cd %UserProfile%
+cd Downloads
+mkdir Ext
+mkdir Ext\Exe
+mkdir Ext\Bin
+mkdir Ext\Jar
+mkdir Ext\Lua
+mkdir Ext\JS
+mkdir Ext\ISO
+mkdir Ext\OBJ
+mkdir Ext\Zip
+mkdir Ext\Videos
+mkdir Ext\Audio
+mkdir Ext\Pictures
+mkdir Ext\osu!
+mkdir Ext\Tetrio
+mkdir Ext\ini
+mkdir Ext\Quaver
+mkdir Ext\docx
+mkdir Ext\Torrent
+mkdir Ext\Virtualbox
+mkdir Ext\msi
+mkdir Ext\yml
+mkdir Ext\Fonts
+mkdir Ext\DLL
+mkdir Ext\txt
+mkdir Ext\efi
+mkdir Ext\HTML
+mkdir Ext\crx
+mkdir "Ext\Linux Applications"
+mkdir Ext\C
+mkdir Ext\CS
+mkdir Ext\Java
+mkdir Ext\db
+
+move *.exe Ext\Exe\
+move *.bin Ext\Bin\
+move *.jar Ext\Jar\
+move *.lua Ext\Lua\
+move *.js Ext\JS\
+move *.iso Ext\ISO\
+move *.obj Ext\OBJ\ 
+move *.zip Ext\Zip\
+move *.7z Ext\Zip\
+move *.rar Ext\Zip\
+move *.tar.gz Ext\Zip\
+move *.mp4 Ext\Videos\
+move *.mov Ext\Videos\
+move *.webm Ext\Videos\
+move *.m4v Ext\Videos\
+move *.mp3 Ext\Audio\
+move *.png Ext\Pictures\
+move *.jpg Ext\Pictures\
+move *.jpeg Ext\Pictures\
+move *.ico Ext\Pictures\
+move *.webp Ext\Pictures\
+move *.jfif Ext\Pictures\
+move *.gif Ext\Pictures\
+move *.svg Ext\Pictures\
+move *.osz Ext\osu!\
+move *.osk Ext\osu!\
+move *.tpse Ext\Tetrio\
+move *.ttrm Ext\Tetrio\
+move *.ttr Ext\Tetrio\
+move *.ini Ext\ini\
+move *.qp Ext\Quaver\
+move *.docx Ext\docx\
+move *.torrent Ext\Torrent\
+move *.magnet Ext\Torrent\
+move *.vbox-extpack Ext\Virtualbox\
+move *.vbox Ext\Virtualbox\
+move *.img Ext\Virtualbox\
+move *.msi Ext\msi\
+move *.yml Ext\yml\
+move *.yaml Ext\yml\
+move *.ttf Ext\Fonts\
+move *.otf Ext\Fonts\
+move *.woff Ext\Fonts\
+move *.fnt Ext\Fonts\
+move *.fon Ext\Fonts\
+move *.eot Ext\Fonts\
+move *.acfm Ext\Fonts\
+move *.amfm Ext\Fonts\
+move *.dfont Ext\Fonts\
+move *.ttc Ext\Fonts\
+move *.dll Ext\DLL\
+move *.txt Ext\txt\
+move *.efi Ext\efi\
+move *.html Ext\HTML\
+move *.crx Ext\crx\
+move *.deb "Ext\Linux Applications"
+move *.appimage "Ext\Linux Applications"
+move *.c Ext\C
+move *.h Ext\C
+move *.cpp Ext\C 
+move *.hpp Ext\C
+move *.cs Ext\CS 
+move *.java Ext\Java
+move *.class Ext\Java
+move *.sql Ext\db
+move *.db Ext\db
+move *.mysql Ext\db
+move *.sqlite Ext\db
